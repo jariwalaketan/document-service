aa96505b7012e653796ab2a47d55b64ce9af317e
Ketan Jariwala <ketan@ketans-MacBook-Pro.local>
2025-04-03 18:20:50 +0530

    excluded target folde

--- target/generated-sources/grpc/com/example/HelloGrpc.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import io.quarkus.grpc.MutinyService;
-
-@jakarta.annotation.Generated(value = "by Mutiny Grpc generator", comments = "Source: hello.proto")
-public interface HelloGrpc extends MutinyService {
-
-    io.smallrye.mutiny.Uni<com.example.HelloReply> sayHello(com.example.HelloRequest request);
-}
--- target/quarkus-grpc356672433680646609.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/Users/ketan/Library/Java/JavaVirtualMachines/corretto-17.0.7/Contents/Home/bin/java" -cp "/Users/ketan/.m2/repository/io/quarkus/quarkus-grpc-protoc-plugin/3.6.4/quarkus-grpc-protoc-plugin-3.6.4-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
